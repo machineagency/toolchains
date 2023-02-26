6b8977d234d011263856c486fed4ae2a360b23bc
branchwelder <htwiggsmith@gmail.com>
2023-02-25 19:56:32 -0800

    Updates

--- assets/toggle-186504bc.js
+++
@@ -1,8 +0,0 @@
-import{y as o}from"./index-59651c4e.js";const n={displayName:"toggle",out:{id:"bool",type:"boolean"},state:{toggled:!0}};function e(t){console.log(t)}function c(t){return o`<style>
-      input[type="checkbox"] {
-        height: 20px;
-        width: 20px;
-        margin: 0;
-      }
-    </style>
-    <input type="checkbox" @click=${e} />`}export{n as config,c as view};
