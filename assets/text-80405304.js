2c044302f4cbe8f08da648dffa2eb67f128c1c61
branchwelder <htwiggsmith@gmail.com>
2023-03-06 15:36:33 -0800

    Updates

---
+++ assets/text-80405304.js
@@ -0,0 +1,9 @@
+import{y as r}from"./index-db930cb1.js";const l={inports:{},outports:{text:{type:"string",value:null}},state:{text:"hello world"},ui:{displayName:"text",width:"200px",height:"50px"}};function u(p,n,e){function i(t,o){o.text=t.target.value,n.text=t.target.value}return{init:()=>{console.log("initializing!!")},render:()=>r`<style>
+        input {
+          width: 100%;
+        }
+      </style>
+      <input
+        type="text"
+        value=${e.text}
+        @input=${t=>i(t,e)} />`}}const c={config:l,tool:u};export{c as default};
--- assets/toggle-24cd7d21.js
+++
@@ -1,6 +0,0 @@
-import{y as i}from"./index-e1425a13.js";const g={inports:{},outports:{toggle:{type:"boolean",value:!1}},state:{toggle:!1},ui:{displayName:"toggle",width:"100px",height:"50px"}};function l(c,o,t){function n(e){t.toggle=e.target.checked,o.toggle=e.target.checked}return{init:()=>{console.log("initializing!!")},render:()=>i`<style>
-        input {
-          width: 100%;
-        }
-      </style>
-      <input type="checkbox" ?checked=${t.toggle} @input=${n} />`}}const u={config:g,tool:l};export{u as default};
