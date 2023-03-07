2f35f3d820e79b04bc2676f67ed4fd4e8c03c2c6
branchwelder <htwiggsmith@gmail.com>
2023-03-06 18:06:01 -0800

    Updates

--- assets/base-9a375af6.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./index-db930cb1.js";const o={inports:{text:{type:"string",value:"hello"}},outports:{},state:{},ui:{displayName:"base",width:"200px",height:"200px"}};function r(t,s,i){return{resize:()=>{},render:()=>e`<div>${t.text}</div>`}}const u={config:o,tool:r};export{u as default};
